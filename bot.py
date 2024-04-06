df8501b903d00953ee14d105c01cc6d40fe14ed8
finique <alex.fedotenkov@alumni.ie.edu>
2024-04-06 15:02:42 +0200

    Initial commit

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
+import os
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Hello')
 
 def main() -> None:
     # Replace 'YOUR_TOKEN_HERE' with your bot's token
-    application = Application.builder().token('YOUR_TOKEN_HERE').build()
+    application = Application.builder().token(os.environ['TG_BOT_TOKEN']).build()
 
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
